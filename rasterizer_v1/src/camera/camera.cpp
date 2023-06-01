1846f553e0a919faca8624aa79a5e83e05ceb73b
Li Jianping <1196934490@qq.com>
2023-06-02 00:03:42 +0800

    Minor changes

--- rasterizer_v1/src/camera/camera.cpp
+++ rasterizer_v1/src/camera/camera.cpp
@@ -33,9 +33,11 @@ void FpsCamera::lookUp(double deg) {
 
 void FpsCamera::lookRight(double deg) {
     auto s = SphericalCoordinates(CartesianCoordinates(lookAt));
+
     s.phi += toRadian(deg);
     if (s.phi > pi) s.phi -= 2 * pi;
     if (s.phi < -pi) s.phi += 2 * pi;
+    
     auto c = CartesianCoordinates(s);
     lookAt = {{c.x, c.y, c.z}};
 }
