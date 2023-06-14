334f191f2cb6f2996f1a7c9851721f52fa0ed2fd
Li Jianping <1196934490@qq.com>
2023-06-14 22:59:59 +0800

    Add a sphere to hit

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -9,11 +9,25 @@ T lerp(const T& a, const T& b, double t) {
     return (1 - t) * a + t * b;
 }
 
-Vec3 rayColor(Ray r) {
+bool hitSphere(const Ray& r, const Vec3& center, double radius) {
+    auto oc = r.o - center;
+    auto a = dot(r.d, r.d);
+    auto b = 2.0 * dot(r.d, oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return discriminant > 0.0;
+}
+
+Vec3 rayColor(const Ray& r) {
+    if (hitSphere(r, Vec3(0, 0, -1), 0.5)) {
+        return Vec3(1, 0, 0);
+    }
     auto u = normalized(r.d);
     auto t = (u.y() + 1.0) * 0.5;
     return lerp(Vec3(1.0, 1.0, 1.0), Vec3(0.5, 0.7, 1.0), t);
 }
+
+
 
 int main() {
     // Image
