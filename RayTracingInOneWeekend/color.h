ac63ecf6ec256c09ef7716e10f33900a483275c5
Li Jianping <1196934490@qq.com>
2023-06-15 16:25:54 +0800

    Gamma correction

--- RayTracingInOneWeekend/color.h
+++ RayTracingInOneWeekend/color.h
@@ -6,10 +6,12 @@
 #include "rtweekend.h"
 
 inline void writeColor(std::ostream& os, const Vec3& color, int samplesPerPixel) {
-    auto output = color;
-    output *= 1.0 / samplesPerPixel;
+    auto r = std::sqrt(color.x() / samplesPerPixel);
+    auto g = std::sqrt(color.y() / samplesPerPixel);
+    auto b = std::sqrt(color.z() / samplesPerPixel);
 
-    os << static_cast<int>(256 * clamp(output.x(), 0.0, 0.999)) << ' '
-       << static_cast<int>(256 * clamp(output.y(), 0.0, 0.999)) << ' '
-       << static_cast<int>(256 * clamp(output.z(), 0.0, 0.999)) << '\n';
+
+    os << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+       << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+       << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
