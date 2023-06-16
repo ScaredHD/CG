10744f65465335f5bf0364cbfbd72ffaf364dae7
Li Jianping <1196934490@qq.com>
2023-06-16 16:02:16 +0800

    Scene with alternate viewpoint

--- RayTracingInOneWeekend/camera.h
+++ RayTracingInOneWeekend/camera.h
@@ -5,25 +5,29 @@
 #include "rtweekend.h"
 
 class Camera {
-public:
-    Camera(double vfov, double aspectRatio) {
+  public:
+    Camera(const Vec3& lookFrom, const Vec3& lookAt, const Vec3& vup, double vfov,
+           double aspectRatio) {
         auto theta = toRadian(vfov);
         auto viewportHeight = 2.0 * std::tan(theta / 2);
         auto viewportWidth = aspectRatio * viewportHeight;
         auto focalLength = 1.0;
 
-        origin = Vec3(0, 0, 0);
-        horizontal = Vec3(viewportWidth, 0, 0);
-        vertical = Vec3(0, viewportHeight, 0);
-        lowerLeft = origin + Vec3(0, 0, -focalLength) - 0.5 * vertical - 0.5 * horizontal;
+        auto w = normalized(lookFrom - lookAt);
+        auto u = normalized(cross(vup, w));
+        auto v = cross(w, u);
 
+        origin = lookFrom;
+        horizontal = viewportWidth * u;
+        vertical = viewportHeight * v;
+        lowerLeft = origin - 0.5 * horizontal - 0.5 * vertical - w;
     }
 
     Ray getRay(double u, double v) const {
         return Ray(origin, lowerLeft + u * horizontal + v * vertical - origin);
     }
 
-private:
+  private:
     Vec3 origin;
     Vec3 lowerLeft;
     Vec3 horizontal;
