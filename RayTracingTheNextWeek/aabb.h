3e519ba9c93cc8c0bb83fa22b84709b46cfb4858
Li Jianping <1196934490@qq.com>
2023-08-01 10:01:30 +0800

    Fixed a bug

--- RayTracingTheNextWeek/aabb.h
+++ RayTracingTheNextWeek/aabb.h
@@ -7,6 +7,7 @@ class AABB {
     AABB() = default;
     AABB(const Vec3& a, const Vec3& b) : a{a}, b{b} {}
 
+    // Doesn't inherent from 'Hittable'
     bool hit(const Ray& r, double tmin, double tmax) const {
         for (int dim = 0; dim < 3; ++dim) {
             auto invD = 1.0 / r.d[dim];
@@ -27,7 +28,7 @@ class AABB {
 inline AABB surroundingBox(const AABB& box0, const AABB& box1) {
     Vec3 a(std::min(box0.a.x(), box1.a.x()), std::min(box0.a.y(), box1.a.y()),
            std::min(box0.a.z(), box1.a.z()));
-    Vec3 b(std::max(box0.b.x(), box1.b.x()), std::max(box0.b.y(), box1.b.z()),
+    Vec3 b(std::max(box0.b.x(), box1.b.x()), std::max(box0.b.y(), box1.b.y()),
            std::max(box0.b.z(), box1.b.z()));
     return {a, b};
 }
